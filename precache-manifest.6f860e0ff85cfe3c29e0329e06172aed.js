33162d80d2ed1c6b10e29157ad3f54d8fa414e75
hamzajaradat999 <hamza.emad.jaradat@gmail.com>
2019-06-10 18:51:40 +0300

    first commit

---
+++ precache-manifest.6f860e0ff85cfe3c29e0329e06172aed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0436efedd643f1465833eb7d00eeb5a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf6da6eb112952f8fd96",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "e60b1b59ebe5f13b571b",
+    "url": "/static/css/main.8ce3110b.chunk.css"
+  },
+  {
+    "revision": "bf6da6eb112952f8fd96",
+    "url": "/static/js/2.bac301d8.chunk.js"
+  },
+  {
+    "revision": "e60b1b59ebe5f13b571b",
+    "url": "/static/js/main.7d68ae09.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7d67987a192a6d95a0e5f900237d16b4",
+    "url": "/static/media/logo.7d67987a.png"
+  }
+]);
